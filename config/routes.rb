01040dd40bd331a505feff1f465291f959729460
dbrett90 <brettd@bc.edu>
2020-11-10 18:37:44 -0500

    Update the form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/faqs', to: 'static_pages#faqs'
+  get '/ticket-appeal-form', to: 'static_pages#ticket_appeal_form'
+  post '/ticket-appeal-form', to: 'static_pages#submit_ticket_appeal_form'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
