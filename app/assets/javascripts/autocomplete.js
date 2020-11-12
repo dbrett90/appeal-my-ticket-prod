e892daf878ba429cccd61c8f65baf2dc483e2d6a
dbrett90 <brettd@bc.edu>
2020-11-12 13:33:49 -0500

    Update the appeal form

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -49,7 +49,7 @@ function initializeAutocomplete(id) {
      var element = document.getElementById(id);
      if (element) {
        var autocomplete = new google.maps.places.Autocomplete(element, { types: ['geocode'], componentRestrictions: {country: 'us'} });
-       google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
+       google.maps.event.addListener(autocomplete, 'place_changed'); //, onPlaceChanged
        }
 }
 
